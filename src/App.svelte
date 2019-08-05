38ded9c349924a42be1453d4ee135c281eccd32f
Eirik Vageskar <eirikvageskar@gmail.com>
2019-08-05 19:29:21 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let name;
+  const yeastAsProportionOfFlour = 0.002;
+  const saltAsProportionOfFlour = 0.03;
+
+  let numberOfDoughBalls = 1;
+  let weightOfEachBall = 250;
+  let waterPercentage = 57;
+
+  $: flour =
+    (numberOfDoughBalls * weightOfEachBall) /
+    (1 +
+      waterPercentage / 100 +
+      yeastAsProportionOfFlour +
+      saltAsProportionOfFlour);
+  $: water = (flour * waterPercentage) / 100;
+  $: salt = flour * saltAsProportionOfFlour;
+  $: yeast = flour * yeastAsProportionOfFlour;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Pizza dough!</h1>
+
+<ul>
+  <li>flour {flour.toFixed(1)}</li>
+  <li>water {water.toFixed(1)}</li>
+  <li>salt {salt.toFixed(1)}</li>
+  <li>yeast {yeast.toFixed(1)}</li>
+</ul>
